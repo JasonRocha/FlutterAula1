2dabad0615a33bd55101ff56a8485a620f53a4e7
Jason Rocha da Silva <jasonrocha3@gmail.com>
2023-07-21 21:51:22 -0300

    exercicio13

--- lib/criando_Widgets/ProductTile.dart
+++ lib/criando_Widgets/ProductTile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutteraula1/Models/productModel.dart';
+import 'package:intl/intl.dart';
 
 class ProductTaile extends StatelessWidget {
   final ProductModel produtos;
@@ -12,7 +13,8 @@ class ProductTaile extends StatelessWidget {
       leading: Image.asset(produtos.fotoProduto),
       title: Text(produtos.nome),
       subtitle: Text(produtos.fabricante),
-      trailing: Text(produtos.valor.toString()),
+      trailing: Text(NumberFormat.currency(locale: 'en-US', decimalDigits: 3)
+          .format(produtos.valor.toString())),
     );
   }
 }
