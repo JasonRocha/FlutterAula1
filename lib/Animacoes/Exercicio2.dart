77142e04e3f7e1dc67267c43777d665a682cb44b
Jason Rocha da Silva <jasonrocha3@gmail.com>
2023-08-04 21:41:47 -0300

    exercicio 2 animation

--- lib/Animacoes/Exercicio2.dart
+++ lib/Animacoes/Exercicio2.dart
@@ -1,9 +1,9 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, avoid_unnecessary_containers
+// ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
-class Exercicio2 extends StatefulWidget {
-  const Exercicio2({super.key});
+/*class Exercicio2 extends StatefulWidget {
+  const Exercicio2({Key? key}) : super(key: key);
 
   @override
   State<Exercicio2> createState() => _Exercicio2State();
@@ -13,21 +13,29 @@ class _Exercicio2State extends State<Exercicio2>
     with SingleTickerProviderStateMixin {
   late AnimationController animationController;
   late Animation<double> animation;
+  bool isLoading = false;
 
   @override
   void initState() {
     super.initState();
 
-    this.animationController =
-        new AnimationController(vsync: this, duration: Duration(seconds: 3));
+    animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 3));
 
-    this.animation = new Tween<double>(begin: 100, end: 500)
-        .animate(this.animationController);
+    animation = Tween<double>(begin: 500, end: 50).animate(animationController);
+
+    animationController.forward().then((_) {
+      setState(() {
+        isLoading = true; // Ativa o sinal de carregamento após a animação
+      });
+    });
+
+    animationController.forward();
   }
 
   @override
   void dispose() {
-    this.animationController.dispose();
+    animationController.dispose();
 
     super.dispose();
   }
@@ -40,18 +48,18 @@ class _Exercicio2State extends State<Exercicio2>
       ),
       body: Center(
         child: Container(
-          width: 500,
+          width: 600,
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              SizedBox(height: 50),
               TextField(
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                   labelText: 'Usuario',
                 ),
               ),
-              SizedBox(
-                height: 50,
-              ),
+              SizedBox(height: 20),
               TextField(
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
@@ -59,10 +67,106 @@ class _Exercicio2State extends State<Exercicio2>
                 ),
               ),
               SizedBox(
-                height: 70,
-              )
+                height: 20,
+              ),
+              AnimatedBuilder(
+                animation: animation,
+                builder: (context, child) {
+                  return Container(
+                    width: animation.value,
+                    height: 50,
+                    color: Colors.blue,
+                    child: Center(child: Text('Login')),
+                  );
+                },
+              ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}*/
+
+class Exercicio2 extends StatefulWidget {
+  const Exercicio2({Key? key}) : super(key: key);
+
+  @override
+  State<Exercicio2> createState() => _Exercicio2State();
+}
+
+class _Exercicio2State extends State<Exercicio2>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<double> animation;
+
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 3));
+
+    animation =
+        Tween<double>(begin: 500, end: 100).animate(animationController);
+
+    animationController.forward().then((_) {
+      setState(() {
+        isLoading = true; // Ativa o sinal de carregamento após a animação
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Exercicio 3'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedBuilder(
+              animation: animation,
+              builder: (context, child) {
+                if (isLoading) {
+                  // Exibe o sinal de carregamento (CircularProgressIndicator)
+                  return CircularProgressIndicator();
+                } else {
+                  // Exibe o retângulo diminuindo de tamanho
+                  return Container(
+                    width: animation.value,
+                    height: 50,
+                    color: Colors.blue,
+                  );
+                }
+              },
+            ),
+            SizedBox(height: 50),
+            TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Usuario',
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Senha',
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/Animacoes/bottao.dart
+++ lib/Animacoes/bottao.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: must_be_immutable
+// ignore_for_file: must_be_immutable, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
 
